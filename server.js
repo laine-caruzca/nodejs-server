b1038a74de5686cc5ccd51b8e53fb6f9dcbc1f25
laine-caruzca <48063178+laine-caruzca@users.noreply.github.com>
2020-04-16 15:07:10 +0100

    v1

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.json({
-    name: "Laine",
-  });
+  // res.json({
+  //   name: "Laine",
+  // });
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 app.listen(PORT, () => console.log("Server is running on PORT " + PORT));
